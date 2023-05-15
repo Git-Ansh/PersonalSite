a08fa2bcd9aa5849f61aeff06a2bd938e1db0569
Ansh Shah <anshjarvis2003@gmail.com>
2023-05-15 01:35:11 -0400

    Delete websiteDevelopment - Copy directory

--- websiteDevelopment - Copy/assets/js/scrollbar.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.querySelector('.scroll-progress');
-window.addEventListener('scroll', () => {
-  const scrolled = window.pageYOffset;
-  const windowHeight = window.innerHeight;
-  const fullHeight = document.body.clientHeight;
-  const progress = (scrolled / (fullHeight - windowHeight)) * 100;
-  progressBar.style.height = `${progress}%`;
-});
